b834c4caf72b2a1192183d860aed0fc31ce95093
Basweti <baswetima@gmail.com>
2019-06-13 22:53:21 +0300

    try

--- resources/views/vendor/swagger-lume/index.blade.php
+++ resources/views/vendor/swagger-lume/index.blade.php
@@ -75,7 +75,7 @@
         const ui = SwaggerUIBundle({
             dom_id: '#swagger-ui',
 
-            url: "{!! $urlToDocs !!}",
+            url: "https://mysterious-lake-67681.herokuapp.com/docs",
             operationsSorter: {!! isset($operationsSorter) ? '"' . $operationsSorter . '"' : 'null' !!},
             configUrl: {!! isset($additionalConfigUrl) ? '"' . $additionalConfigUrl . '"' : 'null' !!},
             validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
